6da8b53adc67422ed285a9b112f0080e341432ef
Divya <divya.venu@gmail.com>
2021-05-05 15:51:33 +0530

    Modifications

---
+++ README.md
@@ -0,0 +1,21 @@
+Steps to setup the project
+
+Clone the project.
+CD into the project
+
+To install dependencies, run 
+> npm install
+
+Modify the path in the program to the API endpoint from where you need the json response
+
+To execute the program , run 
+> npm start
+
+The following responses should be displayed once the program successfully executes:
+
+statusCode: 200
+The file has been saved!
+Reading file!
+Excel created!
+
+A response.json file and a response.xlsx file should be created in the folder.
--- src/index.js
+++ src/index.js
@@ -1,28 +1,56 @@
 const https = require('https')
+const fs = require("fs")
+const xlsx = require("xlsx")
+const filepath ="./response.json"
+
 const options = {
-  hostname: 'www.google.com',
+  hostname: 'google.com',
   port: 443,
-//  path: '',
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  //path: '/test',
+  method: 'GET'
 }
 
 const req = https.request(options, res => {
   console.log(`statusCode: ${res.statusCode}`)
-
+  let body = "";
   res.on('data', d => {
-    process.stdout.write(d)
-    const fs = require('fs');
-     fs.writeFile("output.xlsx",d,(err) => {
-       if (err) throw err;
-      })
-  })
+    body += d;
+  });
+  res.on("end", () => {
+    try {
+          fs.writeFileSync(filepath, body);
+          console.log('The file has been saved!');
+
+          readJsonFile();     
+    } catch (error) {
+        console.error(error.message);
+    };
+});
 })
-console.log('The file has been saved!');
+
 req.on('error', error => {
   console.error(error)
 })
 
 req.end()
+
+function readJsonFile() {
+  console.log('Reading file!');
+  var rawFile = fs.readFileSync(filepath)
+  const raw = JSON.parse(rawFile);
+
+  var files  = []
+  for (each in raw){
+      files.push(raw[each])
+      }  
+    var obj = files.map((e) =>{
+          return e
+        })
+
+  var newWB = xlsx.utils.book_new();
+  var newWS = xlsx.utils.json_to_sheet(obj)
+  xlsx.utils.book_append_sheet(newWB,newWS,"responses")
+  xlsx.writeFile(newWB,"Response.xlsx")
+
+  console.log('Excel created!');
+}
